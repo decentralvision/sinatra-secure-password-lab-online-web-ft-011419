15a1eb814a2201b3bc15bf0e20b974a21d00ae42
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-06 17:18:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     binding.pry
-    if !params["username"].empty? && !params["password"].empty
+    if !params["username"].empty? && !params["password"].empty?
       redirect '/login'
     else
       redirect '/failure'
